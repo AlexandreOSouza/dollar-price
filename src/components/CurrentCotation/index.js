de638b4098488d99d1a58e4cce48c3cb141ccbfc
Alexandre <alexandreSnow>
2021-08-19 12:45:19 -0300

    Real time Dollar

--- src/components/CurrentCotation/index.js
+++ src/components/CurrentCotation/index.js
@@ -3,6 +3,8 @@ import TrendingDownIcon from '@material-ui/icons/TrendingDown';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Puff } from 'react-loading-icons'
+
 
 function CurrentCotation() {
     const coinType = "R$"
@@ -12,27 +14,33 @@ function CurrentCotation() {
     const [ trandingIsUp, setTrandingIsUp] = useState(true)
 
     useEffect(() => {
-        const val = async () => {
+        setInterval(async () => {
             let response = await fetchData()
             setCurrentValue(response.bid)
             let pct = response.pctChange
             setDiffTax(pct + "%")
             setTrandingIsUp(pct > -1)
-        }
-        val()
+
+          }, 10000);
     }, [currentValue])
     
 
     return (
         <div className="current">
+            {(currentValue === 0) ? 
+                <Puff />
+            : 
+            <>
+                <p className="coinType">{coinType}</p>
+                <p className="currentValue">{currentValue}</p>
+                
+                <div className="tranding">
+                    {trandingIsUp ? <TrendingUpIcon className="isUp" /> : <TrendingDownIcon className="isDown" />}
+                    <p className={trandingIsUp ? "isUp" : "isDown"}>{diffTax}</p>
+                </div>
+            </>
+            }
             
-            <p className="coinType">{coinType}</p>
-            <p className="currentValue">{currentValue}</p>
-            
-            <div className="tranding">
-                {trandingIsUp ? <TrendingUpIcon className="isUp" /> : <TrendingDownIcon className="isDown" />}
-                <p className={trandingIsUp ? "isUp" : "isDown"}>{diffTax}</p>
-            </div>
             
         </div>
     )
